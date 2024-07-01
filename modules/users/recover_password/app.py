aaf689c37e164ac9db3438dae0129cad46e3d217
DaddyDany <20213tn101@utez.edu.mx>
2024-07-01 14:53:21 -0600

    [UPDATE] second try to fix code duplication

--- modules/users/recover_password/app.py
+++ modules/users/recover_password/app.py
@@ -1,6 +1,6 @@
 import boto3
 import json
-from common_functions import get_secret, get_secret_hash
+from common.common_functions import get_secret, get_secret_hash
 
 
 def lambda_handler(event, context):
