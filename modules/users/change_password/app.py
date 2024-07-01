92fa579bf9e0fa7518e952850dde9acd22902978
DaddyDany <20213tn101@utez.edu.mx>
2024-07-01 14:07:41 -0600

    [ADD] Change password lambda

---
+++ modules/users/change_password/app.py
@@ -0,0 +1,77 @@
+import boto3
+import hmac
+import base64
+import hashlib
+import json
+from secrets_manager import get_secret_value
+
+
+def get_secret_hash(username, client_id, client_secret):
+    message = username + client_id
+    dig = hmac.new(client_secret.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).digest()
+    return base64.b64encode(dig).decode()
+
+
+def get_secret():
+    secret_name = "users_pool/client_secret"
+    secret = get_secret_value(secret_name)
+    return secret
+
+
+def lambda_handler(event, context):
+    client = boto3.client('cognito-idp')
+    secret = get_secret()
+
+    body = json.loads(event['body'])
+    username = body['username']
+    confirmation_code = body['confirmation_code']
+    new_password = body['new_password']
+    confirm_new_password = body['confirm_new_password']
+    client_id = '4iid9n3o306aorf0imcs0dcplo'
+    client_secret = secret['SECRET_CLIENT']
+
+    if new_password != confirm_new_password:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('New password and confirmation password do not match.')
+        }
+
+    try:
+        secret_hash = get_secret_hash(username, client_id, client_secret)
+
+        response = client.confirm_forgot_password(
+            ClientId=client_id,
+            Username=username,
+            ConfirmationCode=confirmation_code,
+            Password=new_password,
+            SecretHash=secret_hash
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Password has been reset successfully.')
+        }
+    except client.exceptions.CodeMismatchException:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Invalid confirmation code.')
+        }
+    except client.exceptions.ExpiredCodeException:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Confirmation code has expired.')
+        }
+    except client.exceptions.InvalidPasswordException as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps(f'Invalid password: {e.response["Error"]["Message"]}')
+        }
+    except client.exceptions.UserNotFoundException:
+        return {
+            'statusCode': 404,
+            'body': json.dumps('User not found.')
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('An error occurred while resetting the password: ' + str(e))
+        }
---
+++ modules/users/change_password/secrets_manager.py
@@ -0,0 +1,21 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+
+def get_secret_value(secret_name, region_name="us-east-2"):
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
