8cdb4dcf4e2f2a193454796d425fc3770abbf0d1
DaddyDany <20213tn101@utez.edu.mx>
2024-07-01 11:53:00 -0600

    [UPDATE] Secrets management

--- modules/users/recover_password/app.py
+++ modules/users/recover_password/app.py
@@ -1,9 +1,9 @@
-import boto3
-import json
 import hmac
+import boto3
 import hashlib
+import json
 import base64
-from botocore.exceptions import ClientError
+from secrets_manager import get_secret_value
 
 
 def get_secret_hash(username, client_id, client_secret):
@@ -14,24 +14,8 @@ def get_secret_hash(username, client_id, client_secret):
 
 def get_secret():
     secret_name = "users_pool/client_secret"
-    region_name = "us-east-2"
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        raise e
-
-    secret = get_secret_value_response['SecretString']
-
-    return json.loads(secret)
+    secret = get_secret_value(secret_name)
+    return secret
 
 
 def lambda_handler(event, context):
@@ -48,13 +32,30 @@ def lambda_handler(event, context):
             UserPoolId='us-east-2_bjlyJabGh',
             Username=username
         )
+
         email = next((attr['Value'] for attr in user['UserAttributes'] if attr['Name'] == 'email'), None)
-        secret_hash = get_secret_hash(email, client_id, client_secret)
+        email_verified = next((attr['Value'] for attr in user['UserAttributes'] if attr['Name'] == 'email_verified'),
+                              'false')
+        user_status = user['UserStatus']
+
         if not email:
             return {
                 'statusCode': 404,
                 'body': json.dumps('User email not found.')
             }
+
+        if email_verified != 'true':
+            return {
+                'statusCode': 400,
+                'body': json.dumps('User email not verified.')
+            }
+
+        if user_status != 'CONFIRMED':
+            return {
+                'statusCode': 400,
+                'body': json.dumps("User account hasn't been confirmed.")
+            }
+
     except client.exceptions.UserNotFoundException:
         return {
             'statusCode': 404,
@@ -66,8 +67,9 @@ def lambda_handler(event, context):
             'body': json.dumps('An error occurred while checking user existence: ' + str(e))
         }
 
-    # Iniciar recuperación de contraseña
     try:
+        secret_hash = get_secret_hash(email, client_id, client_secret)
+
         response = client.forgot_password(
             ClientId=client_id,
             Username=email,
