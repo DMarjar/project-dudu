d87291a17cf6632cc06a48e286526187056063e4
CarmonaMedinaMaycon <105668860+CarmonaMedinaMaycon@users.noreply.github.com>
2024-08-09 16:52:04 -0600

    fix complete mission

--- modules/missions/complete_mission/app.py
+++ modules/missions/complete_mission/app.py
@@ -1,4 +1,5 @@
 import json
+import random
 from common.db_connection import get_db_connection
 
 
@@ -11,53 +12,89 @@ def lambda_handler(event, __):
     try:
         body = json.loads(event['body'])
         if not body:
-            response = {
+            return {
                 'statusCode': 400,
                 'headers': headers,
                 'body': json.dumps({"message": "Bad request: Body is required"})
             }
-            return response
 
         id_mission = body.get('id_mission')
+        id_user = body.get('id_user')
 
-        if id_mission is None:
-            response = {
+        if id_mission is None or id_user is None:
+            return {
                 'statusCode': 402,
                 'headers': headers,
-                'body': json.dumps({"message": "Bad request: id of mission is required"})
+                'body': json.dumps({"message": "Bad request: id of mission and user is required"})
             }
-            return response
 
-        if isinstance(id_mission, int):
-            update_mission_status(id_mission, 'completed')
-            response = {
-                'statusCode': 200,
-                'headers': headers,
-                'body': json.dumps({"message": f"Mission {id_mission} completed successfully"})
-            }
+        if isinstance(id_mission, int) and isinstance(id_user, int):
+            # Inicia la transacción
+            connection = get_db_connection()
+            try:
+                with connection.cursor() as cursor:
+                    #Verificar que el current_xp no sea mayor al xp_limit
+                    cursor.execute("SELECT current_xp, xp_limit, level FROM users WHERE id_user = %s FOR UPDATE", (id_user,))
+                    user = cursor.fetchone()
+                    if not user:
+                        raise Exception("User not found")
+
+                    current_xp, xp_limit, level = user
+
+                    if current_xp >= xp_limit:
+                        raise Exception("User's XP is already at the limit")
+
+                    # Crear un número random entre 1 y 25
+                    random_xp = random.randint(1, 25)
+
+                    # Completar la misión
+                    cursor.execute("UPDATE missions SET status = 'completed' WHERE id_mission = %s", (id_mission,))
+
+                    # Sumar el número random al current_xp
+                    new_current_xp = current_xp + random_xp
+
+                    # Verificar si al sumar el XP excede el límite
+                    if new_current_xp >= xp_limit:
+                        # Subir de nivel y resetear el XP
+                        new_level = level + 1
+                        new_current_xp = new_current_xp - xp_limit
+                        new_limit_xp = xp_limit + 10
+                        cursor.execute("UPDATE users SET level = %s, current_xp = %s, xp_limit = %s WHERE id_user = %s", (new_level, new_current_xp, new_limit_xp , id_user))
+                    else:
+                        # actualizar el XP
+                        cursor.execute("UPDATE users SET current_xp = %s WHERE id_user = %s", (new_current_xp, id_user))
+
+                    connection.commit()
+
+                response = {
+                    'statusCode': 200,
+                    'headers': headers,
+                    'body': json.dumps({"message": f"Mission {id_mission} completed successfully and XP updated"})
+                }
+
+            except Exception as e:
+                connection.rollback()
+                response = {
+                    'statusCode': 500,
+                    'headers': headers,
+                    'body': json.dumps({"message": f"An error occurred: {str(e)}"})
+                }
+
+            finally:
+                connection.close()
+
         else:
             response = {
                 'statusCode': 404,
                 'headers': headers,
-                'body': json.dumps({"message": "Mission not found"})
+                'body': json.dumps({"message": "Invalid mission or user ID"})
             }
 
     except Exception as e:
         response = {
             'statusCode': 500,
             'headers': headers,
-            'body': json.dumps({"message": f"An error occurred while completing the mission: {str(e)}"})
+            'body': json.dumps({"message": f"An error occurred: {str(e)}"})
         }
+
     return response
-
-
-def update_mission_status(id_mission, status):
-    connection = get_db_connection()
-    try:
-        with connection.cursor() as cursor:
-            # Actualiza el estado de la misión
-            sql_update = "UPDATE missions SET status = %s WHERE id_mission = %s"
-            cursor.execute(sql_update, (status, id_mission))
-            connection.commit()
-    finally:
-        connection.close()
