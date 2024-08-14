c4d67016dfbb32dd33ed7fbad4d0fcef2d82bbe6
Maycon Manuel Carmona Medina <20213tn004@utez.edu.mx>
2024-08-13 21:30:55 -0600

    ?

--- modules/missions/complete_mission/app.py
+++ modules/missions/complete_mission/app.py
@@ -70,10 +70,10 @@ def lambda_handler(event, __):
                                 (new_level, new_current_xp, new_limit_xp, id_user))
 
                             max_reward_id = 11
-                            if  new_level % 5 == 0: 
+                            if new_level % 5 == 0:
                                 cursor.execute("SELECT id_reward FROM user_rewards WHERE id_user = %s", (id_user,))
-                                reward_increment = cursor.fetchone()
-                                reward_increment + 1 
+                                reward_increment = cursor.fetchone()[0]  # Asegúrate de obtener el valor de la columna
+                                reward_increment += 1
                         
                             if reward_increment > 0:
                                 new_reward_id = min(reward_increment, max_reward_id)
