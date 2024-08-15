9f555a9f02ac86a1b9dc861736a61a4b6dabc24b
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2024-08-15 11:48:25 -0600

    [FIX] modified paths

--- modify_imports.sh
+++ modify_imports.sh
@@ -6,13 +6,13 @@ if [ "$1" == "test" ]; then
   # Para cada carpeta (missions, profile, users)
   for module in missions profile users; do
     # Buscar todos los archivos Python y modificar las importaciones
-    find modules/$module -name "*.py" -exec sed -i 's/from common/from modules.'"$module"'.\0/g' {} +
+    find modules/$module -name "*.py" -exec sed -i "s/from common/from modules.$module/g" {} +
   done
 
 elif [ "$1" == "deploy" ]; then
   # Para cada carpeta (missions, profile, users)
   for module in missions profile users; do
     # Restaurar las importaciones originales para el despliegue
-    find modules/$module -name "*.py" -exec sed -i 's/from modules.'"$module"'\.\(.*\)\.common/from common/g' {} +
+    find modules/$module -name "*.py" -exec sed -i "s/from modules.$module.common/from common/g" {} +
   done
 fi
