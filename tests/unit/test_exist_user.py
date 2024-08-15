4a2010bc7da6961d19d43da011d093992aedeb99
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2024-08-15 13:21:13 -0600

    [FIX] add decorator on test

--- tests/unit/test_exist_user.py
+++ tests/unit/test_exist_user.py
@@ -12,9 +12,17 @@ EVENT = {
 
 
 class Test(unittest.TestCase):
-    def test_lambda_handler(self):
+
+    @patch('modules.users.exist_user.app.get_db_connection')
+    def test_lambda_handler(self, mock_get_db_connection):
+        mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+
+        mock_get_db_connection.return_value = mock_connection
+        mock_connection.cursor.return_value = mock_cursor
+
         response = app.lambda_handler(EVENT, None)
-        self.assertIn(response['body'], ['true', 'false'])
+        self.assertIn(response['body'], ['true', 'false', True, False])
 
     def test_id_user_not_in_body(self):
         event_no_id_user = {
