bbbc1e7061dd66900e4eb4ce1e4ed2ce401a3ca6
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2024-08-16 04:49:23 -0600

    [FIX] register alexa user tests

--- tests/unit/test_register_alexa_user.py
+++ tests/unit/test_register_alexa_user.py
@@ -4,8 +4,44 @@ from unittest.mock import patch, MagicMock
 from modules.users.register_alexa_user import app
 
 
+FAKE_SECRET = {'SecretString': json.dumps({
+    'SECRET_CLIENT': 'client',
+    'ID_CLIENT': 'id',
+    'USER_POOL_ID': 'pool'
+})}
+
+
+# To test_lambda_handler
+class FakeSessionTestLambdaHandler:
+    """
+    FakeSession class to mock boto3 session
+    """
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def client(self, *args, **kwargs):
+        return FakeBoto3ClientTestLambdaHandler()
+
+
+class FakeBoto3ClientTestLambdaHandler:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_secret_value(self, *args, **kwargs):
+        return FAKE_SECRET
+
+
 class Test(unittest.TestCase):
-    def test_lambda_handler(self):
+    @patch('modules.users.register_alexa_user.app.get_db_connection')
+    def test_lambda_handler(self, mock_get_db_connection):
+        mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+
+        mock_get_db_connection.return_value = mock_connection
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor.fetchone.return_value = ['aprendiz de metodología magícaaa']
+
         event = {
             'body': json.dumps({
                 'id_user': 'some-unique-id',
@@ -14,7 +50,7 @@ class Test(unittest.TestCase):
         }
 
         response = app.lambda_handler(event, None)
-        self.assertEqual(response['body'], 'aprendiz de metodología magíca')
+        self.assertEqual(response['body'], 'aprendiz de metodología magícaaa')
 
     @patch('modules.users.register_alexa_user.app.save_user_db')
     def test_save_user_db_exception(self, mock_save_user_db):
@@ -31,7 +67,11 @@ class Test(unittest.TestCase):
         self.assertEqual(response['body'], '"Error inserting user"')
 
     @patch('modules.users.register_alexa_user.app.give_basic_rewards')
-    def test_give_basic_rewards_exception(self, mock_give_basic_rewards):
+    @patch('modules.users.register_alexa_user.app.get_db_connection')
+    def test_give_basic_rewards_exception(self, mock_get_db_connection, mock_give_basic_rewards):
+        mock_connection = MagicMock()
+        mock_get_db_connection.return_value = mock_connection
+
         mock_give_basic_rewards.side_effect = Exception("Error giving basic rewards")
 
         event = {
@@ -100,11 +140,14 @@ class Test(unittest.TestCase):
         self.assertEqual(response['statusCode'], 400)
         self.assertEqual(response['body'], '"id_user is required"')
 
-    @patch('modules.users.register_alexa_user.app.get_first_title')
+    @patch('modules.users.register_alexa_user.app.get_db_connection')
     def test_get_first_title_exception(self, mock_get_db_connection):
         mock_connection = MagicMock()
+        mock_cursor = MagicMock()
+
         mock_get_db_connection.return_value = mock_connection
-        mock_connection.cursor.side_effect = Exception('Error getting first title')
+        mock_connection.cursor.return_value.__enter__.return_value = mock_cursor
+        mock_cursor.fetchone.side_effect = Exception('Error getting first title')
 
         event = {
             'body': json.dumps({
@@ -114,7 +157,7 @@ class Test(unittest.TestCase):
         }
 
         response = app.lambda_handler(event, None)
-        self.assertEqual(response['body'], '"An error occurred: Error getting first title"')
+        self.assertEqual(response['body'], '"Error getting first title"')
 
 
 if __name__ == '__main__':
